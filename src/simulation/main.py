f5755c79038074479c3df09561012d5273900b51
doworder <doworder@gmail.com>
2025-05-03 06:59:06 +0300

    fix map render

--- src/simulation/main.py
+++ src/simulation/main.py
@@ -75,7 +75,7 @@ class Simulation:
 
         Карту
         Счётчик ходов
-        Рендерер поля
+        Рендер поля
         Actions - список действий, исполняемых перед
         стартом симуляции или на каждом ходу"""
 
@@ -84,7 +84,7 @@ class Simulation:
         self._counter = 0
 
     def next_turn(self):
-        """ росимулировать и отрендерить один ход"""
+        """ Просимулировать и отрендерить один ход"""
 
     def start_simulation(self):
         """- запустить бесконечный цикл симуляции и рендеринга"""
@@ -96,20 +96,20 @@ class Simulation:
         width = self.map.width
         height = self.map.height
         rendering_symbols = {
-            Predator: 'P',
-            Herbivore: 'H',
-            Grass: 'G',
-            Rock: 'R',
-            Tree: 'T'
+            Predator: 'P ',
+            Herbivore: 'H ',
+            Grass: 'G ',
+            Rock: 'R ',
+            Tree: 'T '
         }
 
         for i in range(height):
             for j in range(width):
                 coord = Coordinates(i, j)
                 if coord not in self.map.entities:
-                    print('*', end='')
+                    print('* ', end='')
                 else:
-                    print(rendering_symbols[self.map.entities[coord]], end='')
+                    print(rendering_symbols[(self.map.entities[coord]).__class__], end='')
             print()
 
 

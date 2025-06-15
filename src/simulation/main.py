5b4705a770fb14d45d394168264d9f5e33c77118
doworder <doworder@gmail.com>
2025-06-15 20:54:12 +0300

    ref class FindDeadEntity

--- src/simulation/main.py
+++ src/simulation/main.py
@@ -337,15 +337,15 @@ class FindDeadEntity(Actions):
         self.map_object = map_object
 
     @staticmethod
-    def is_dead(entity) -> bool:
-        if entity.hp > 0:
-            return False
-        return True
+    def _is_dead(entity) -> bool:
+        if entity.hp <= 0:
+            return True
+        return False
 
     def __call__(self):
         creatures = self.map_object.get_creatures()
         for entity in creatures:
-            if self.is_dead(entity):
+            if self._is_dead(entity):
                 point = self.map_object.get_entity_point(entity)
                 self.map_object.remove_entity(point)
 

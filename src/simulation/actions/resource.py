f7e2ccdaf23f32b242bdd5975f06d8fd8c1c4951
doworder <doworder@gmail.com>
2025-06-25 22:13:23 +0300

    ref class ResourceBalancer

--- src/simulation/actions/resource.py
+++ src/simulation/actions/resource.py
@@ -5,7 +5,8 @@ from simulation.world_map import Map
 
 class ResourceBalancer(Actions):
     """Добавляет ресурсов, если их осталось мало"""
-    def __init__(self, world_map: Map, resource: type[Entity], action: Actions):
+    def __init__(self, world_map: Map, resource: type[Entity], action: Actions, remaining_count: int):
+        self.remaining_count = remaining_count
         self.action = action
         self.world_map = world_map
         self.resource = resource
@@ -15,5 +16,5 @@ class ResourceBalancer(Actions):
                                 for entity in self.world_map.get_all_entities()
                                 if isinstance(entity, self.resource)]
 
-        if len(all_current_resource) < 3:
+        if len(all_current_resource) < self.remaining_count:
             self.action()
